1b6de56e170f55683476426d19113b5e0fab15db
nashnashnashnashnash <nashnashnashnashnash@localhost.localdomain>
2018-02-03 16:02:18 -0500

    Skeleton for the Client Socket class

---
+++ src/main/java/ClientSocket.java
@@ -0,0 +1,32 @@
+class ClientSocket{
+
+	//Sends the server data on files present
+	sendMetadata(){
+		
+	}
+
+	//Receives a list of available files from the server
+	receiveFileList(){
+
+	}
+
+	//Sends a file request to the server
+	sendRequest(){
+
+	}
+
+	//Receives the IPs of corresponding servers to the server
+	receiveIPs(){
+
+	}
+
+	//Assigns tasks to the peer servers
+	assignTasks(){
+
+	}
+
+	//Sends data corresponding to assigned tasks
+	sendData(){
+
+	}
+}
