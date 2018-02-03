2fc812cc921c80e17664a1f532dedb894a7a6525
nashnashnashnashnash <nashnashnashnashnash@localhost.localdomain>
2018-02-03 18:13:08 -0500

    Added to the server direct download method:

--- src/main/java/ClientSocket.java
+++ src/main/java/ClientSocket.java
@@ -25,7 +25,7 @@ class ClientSocket extends Socket{
 			for (int f = 0 ; f < files.length() ; f++){
 				if (files[f].hashCode() == hash){
 					try{
-						writer = new PrintWriter(getOutputAddress());
+						writer = new PrintWriter(new FileWriter(getOutputAddress()));
 						writer.println (true);
 						writer.close();
 						return true;
@@ -46,11 +46,11 @@ class ClientSocket extends Socket{
 	String requestFileList(){
 		String list = "";
 			try{
-                              writer = new PrintWriter(getOutputAddress());
+                              writer = new PrintWriter(new FileWriter(getOutputAddress()));
                                //Signal the server for file list
 			       writer.println ("f");
                                writer.close();
-				reader = new PrintReader (getInputAddress());
+				reader = new BufferedReader (new FileReader(getInputAddress()));
 				try{
 					while (true){
 						list += reader.readLine()+'\n';
@@ -72,11 +72,11 @@ class ClientSocket extends Socket{
 	//Sends a file request to the server
 	boolean sendRequest(String filename){
 		try{
-			writer = new PrintWriter(getOutputAddress());
+			writer = new PrintWriter(new FileWriter(getOutputAddress()));
 			//Signal the server for get
 			writer.println("g" + filename);
 			writer.close();
-			reader = new PrintReader(getInputAddress());
+			reader = new BufferedReader(new FileReader(getInputAddress()));
 			return Boolean.parseBoolean(reader.readLine());
 		} catch (UnknownHostException e){
 			return null;
@@ -85,6 +85,29 @@ class ClientSocket extends Socket{
 			return null;
 			System.out.println("IO Error: " + e.getMessage());
 		}
+	}
+
+	//Downloads directly from the server
+	servDirect(String filename){
+		try{
+			writer = new PrintWriter(new FileWriter(getOutputAddress()));
+			//Signals server for a direct download
+			writer.println("d"+filename);
+			writer.close();
+			File f = new File("local_files/"+filename);
+			if (f.createNewFile()){
+				writer = new PrintWriter(new FileWriter("local_files"+filename));
+				reader = new BufferedReader(new FileReader(getInputAddress()));
+				try{
+					while (true){
+						char[] buffer[];
+
+						writer.print();
+					}
+				}
+			}
+		}
+		return false;
 	}
 
 	//Receives the IPs of corresponding servers to the server
@@ -108,7 +131,10 @@ class ClientSocket extends Socket{
 		
 		//threaded
 		while (/*ACTIVE*/){
-			
+			if(/* TRIGGERED TO SEND REQUEST*/){
+				if (sendRequest(filename))
+					receiveIPs(filename);
+			}
 		}
 	}
 }
