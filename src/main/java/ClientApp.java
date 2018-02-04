9745436bb881b2829e95dcd315e2125483c48082
nashnashnashnashnash <nashnashnashnashnash@localhost.localdomain>
2018-02-04 07:19:58 -0500

    Made compatible with made program

--- src/main/java/ClientApp.java
+++ src/main/java/ClientApp.java
@@ -5,19 +5,29 @@ import java.net.Socket;
 import java.net.*;
 import java.nio.ByteBuffer;
 import java.util.Scanner;
+	private final byte[] SERVERIP = {(byte)172, (byte)17, (byte)51, (byte)43}
+	public boolean confirmFile(String filename){
+		try{
+			Socket client = new Socket(InetAddress.getByAddress(SERVERIP),8008);
+			PrintWriter output = new PrintWriter (client.getOutputStream());
+		if(new File ("local_files/"+filename).exists()){
+			output.print("true");
+		return true;} else {
+			output.print ("false");
+		return false;}
+		} catch (Exception e) {System.out.println (e.geMessage());}
+	}
 
-public class ClientApp {
-
-    public static void main(String[] args) {
+    private void sendFile(String filename, byte[] IP, int num, int denom) {
         int biggestPack;
         RandomAccessFile input = null;
-        int packNum = 0;
+        int packNum = num;
         try {
-            Socket client = new Socket(InetAddress.getByAddress(new byte[]{(byte) 172, (byte) 17, (byte) 51, (byte) 231}), 4321);
+            Socket client = new Socket(InetAddress.getByAddress(IP, 4321);
             ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
 
             while(true){
-                input = new RandomAccessFile(new File("drums.wav"), "r");
+                input = new RandomAccessFile(new File(filename), "r");
                 biggestPack = (int) Math.ceil(input.length() / 1024.0);
                 input.setLength(biggestPack * 1024);
                 byte[] data = new byte[1024];
@@ -28,7 +38,7 @@ public class ClientApp {
                 out.writeObject(p);
 
                 if (packNum < biggestPack)
-                    packNum++;
+                    packNum+= denom + 1;
                 else
                     break;
             }
@@ -37,6 +47,24 @@ public class ClientApp {
 
         } catch (IOException e){
             System.out.println(e.getMessage());
-        }catch (Exception e){System.out.println("fuck");}
+        }catch (Exception e){System.out.println("ERROR");}
     }
+
+	public static void main(String[] args){
+		ServerSocket listener = new ServerSocket(4321);
+		Socket client = listener.accept();
+		while (true){
+		try{
+			BufferedReader input = new BufferedReader(client.getInputStream());
+			String filename = input.readline();
+			if (confirmFile(filename)){
+				String[] IP = input.readLine().split(".");
+				byte[] ip = new byte[IP.length];
+				for (int i = 0 ; i < ip.length ; i++){
+					ip[i] = Byte.parseByte(IP[i]);
+				int num = Integer.parseInt(input.readLine());
+				sendFile (filename, ip, num, Integer.parseInt(input.readLine()));
+			}
+		}
+	} catch (Exception e){System.out.println(e.getMessage());}}
 }
